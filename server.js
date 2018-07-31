af3d00a2ddc3bb11d596a19dbf390973ffbcd745
Daniel lara <daniellaraedinburgh@hotmail.com>
2018-08-01 00:45:48 +0100

    all routes working

--- server.js
+++ server.js
@@ -7,6 +7,22 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/client/index0.html');
 });
 
+app.get('/resume', function(req, res){
+  res.sendFile(__dirname + '/client/index-1.html');
+});
+
+app.get('/contact', function(req, res){
+  res.sendFile(__dirname + '/client/index-2.html');
+});
+
+app.get('/about', function(req, res){
+  res.sendFile(__dirname + '/client/index-3.html');
+});
+
+app.get('/projects', function(req, res){
+  res.sendFile(__dirname + '/client/index-4.html');
+});
+
 app.use(express.static('client'));
 app.use(bodyParser.json());
 
